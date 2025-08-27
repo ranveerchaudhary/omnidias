48ec3c7fbf328e122724b4c7624d3ddd94317b07
Ranveer Chaudhary <ranveerjerry@gmail.com>
2025-08-28 02:14:12 +0530

    basic tokenization and exploring rust - [1]

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 use std::time::Instant;
 use std::io;
 use std::fs;
-mod query_parser;
+mod parser;
 
-fn store_vectors() -> io::Result<()> {
-    let vectors: [f64; 3] = [0.45, 0.355532, 0.2456];
-
-    // Convert array to a comma-separated string
-    let content = vectors.iter()
-                         .map(|v| v.to_string())
-                         .collect::<Vec<String>>()
-                         .join(", ");
-
-    // Write to file, propagate error if any
-    fs::write("omnidias.omda", content)?;
-
-    // Print the vectors
-    println!("These are the vectors: {:?}", vectors);
-
-    Ok(())
-}
-
-fn parse_query(query: &str) -> String{
-    return "".to_string();
-}
 
 fn main() {
     // Handle potential error
     let query = String::from("SELECT VECTORS FROM VECTABLE WHERE CLOSEST [[0.3345343, 0.435454645, 0.45465656]] AND METHOD IS HSNW");
-    println!("{:#?}", query_parser::parse_query(&query));
+    let tokenizedquery = parser::tokenizer::tokenizer(&query);
+    parser::treebuilder::treebuilder(&tokenizedquery);
 }
---
+++ src/parser/treebuilder.rs
@@ -0,0 +1,6 @@
+
+pub fn treebuilder(tokenized_query: &Vec<&str>){
+
+    println!("{:?}", tokenized_query);
+
+}
